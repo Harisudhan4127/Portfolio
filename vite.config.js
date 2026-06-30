cf29002f6eb4e4fb8685da47578b81f80989e27b
Harisudhan4127 <harisudhan4127@gmail.com>
2026-06-30 16:59:25 +0530

    30/06/2026

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './' // Set base path for GitHub Pages docs folder
+  base: '/Portfolio/' // ✅ Use repository name for base path
 });
